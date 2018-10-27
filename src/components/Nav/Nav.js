39aca1af1937a3b909db5c298d13d6ebc6f2a198
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-10-27 15:27:27 -0500

    fixed navbar and npm installed filestack.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import { BottomNavigation, BottomNavigationAction, Button, Grid } from '@material-ui/core';
+
 
 const Nav = (props) => (
   <div className="nav">
@@ -14,7 +16,7 @@ const Nav = (props) => (
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
+        {props.user.id ? 'Dash' : 'Login / Register'}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
