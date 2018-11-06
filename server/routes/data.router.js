93ba7129087fe6de1c37cc04a6bd2939fb77cb7c
k-burn <karaburnett93@gmail.com>
2018-11-06 11:39:31 -0600

    Added a route to get the light readings every five minutes

--- server/routes/data.router.js
+++ server/routes/data.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const cron = require('node-cron');
+const axios = require('axios');
 
 // GET route for Plant Data (History) View -- 
 // gets all plant data and displays it on the page, ordered by intake date
@@ -18,4 +20,20 @@ router.get('/', (req, res) => {
     };//end if else
 }); // end plant data GET route
 
+// Make a request for data every 10 minutes
+cron.schedule('*/5 * * * *', function(){
+    console.log('running a task every 5 minutes');
+    getLight();
+});
+
+getLight();  
+
+function getLight(){
+    axios.get(`https://api.spark.io/v1/devices/${process.env.DEVICE_ID}/front?access_token=${process.env.TOKEN}`).then(function (response) {
+      // TODO: Save results in the database
+      console.log(response.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
+} 
 module.exports = router;
